215fcaf19d5c2d54314c130b6f76344534d71249
J. J. W. Mezun <jjwmezun@gmail.com>
2022-01-28 09:17:38 -0800

    Add code to delete and create tables in database

--- server/build.js
+++ server/build.js
@@ -5,10 +5,12 @@ const path = require( `path` );
 require( `dotenv` ).config( { path: path.resolve( process.cwd(), '.env.admin' ) } );
 
 db.connect();
-db.clearPosts().then( () => {
-	loadPosts().then( ( posts ) => {
-		Promise.all( posts.map( ( post ) => db.createPost( post ) ) ).then( () => {
-			db.close();
+db.initTables().then( () => {
+	db.clearPosts().then( () => {
+		loadPosts().then( ( posts ) => {
+			Promise.all( posts.map( ( post ) => db.createPost( post ) ) ).then( () => {
+				db.close();
+			})
 		})
 	})
 });
--- src/db.js
+++ src/db.js
@@ -3,6 +3,30 @@ const createPost = require( `./post` );
 
 let client;
 
+const tables = [
+    {
+        name: `post`,
+        columns: {
+            title: `text`,
+            content: `text`,
+            slug: `text`,
+            pubdate: `timestamp`
+        }
+    },
+    {
+        name: `category`,
+        columns: {
+            title: `text`,
+            slug: `text`
+        }
+    },
+    {
+        name: `post_category`,
+        columns: {},
+        references: [ `post` ]
+    }
+];
+
 module.exports = {
     connect: () => {
         client = new Client({
@@ -16,6 +40,31 @@ module.exports = {
     close: () => {
         client.end();
     },
+    initTables: () => Promise.all( tables.reverse().map( table => new Promise( ( resolve, reject ) => {
+        client.query( `drop table if exists ${ table.name }`, ( err, res ) => {
+            if ( err ) {
+                reject( err );
+            }
+            else {
+                /*
+                const columns = [ `${ table.name }_id serial primary key`].concat( Object.keys( table.columns ).map( key => `${ key } ${ table.columns[ key ] }`) );
+                if ( table.references ) {
+                    columns.push( `${ table.references[ 0 ] }_id int` );
+                    columns.push( `constraint fk_${ table.references[ 0 ] } foreign key( ${ table.references[ 0 ] }_id ) references ${ table.references[ 0 ] }( ${ table.references[ 0 ] }_id )` );
+                }
+                client.query( `create table ${ table.name } ( ${ columns.join( `, ` ) } )`, ( err, res ) => {
+                    if ( err ) {
+                        reject( err );
+                    }
+                    else {
+                        resolve( res );
+                    }
+                });
+                */
+               resolve( res );
+            }
+        });
+    }))),
     clearPosts: () => new Promise( ( resolve, reject ) => {
         client.query( `truncate post`, ( err, res ) => {
             if ( err ) {
